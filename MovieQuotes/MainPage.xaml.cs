b7f996b5d3394dae990977592929a915d6711481
dotnetcurry <dotnetcurry@a2zknowledgevisuals.com>
2013-08-31 10:43:36 +0530

    Code Commit

---
+++ MovieQuotes/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using MovieQuotes.Resources;
+
+namespace MovieQuotes
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        //Private variables to hold movie quotes
+        private List<MovieQuote> _movieQuotes;
+        private QuotesReader _reader;
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            Loaded += MainPageLoaded;
+        }
+        //When page is loaded, read quotes using reader
+        //And bind it to main page
+        private void MainPageLoaded(object sender, RoutedEventArgs e)
+        {
+            _reader = new QuotesReader();
+            _movieQuotes = _reader.GetMovieQuotes();
+            this.DataContext = _movieQuotes.FirstOrDefault();
+        }
+        //Handle previous quote button click
+        private void BackClicked(object sender, EventArgs e)
+        {
+           this.DataContext = _reader.GetMovieQuotes().GetSingleRandom();
+        }
+        //Handle next quote button click
+        private void NextClicked(object sender, EventArgs e)
+        {
+            this.DataContext = _reader.GetMovieQuotes().GetSingleRandom();
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
---
+++ MovieQuotes/QuotesReader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MovieQuotes
+{
+    /// <summary>
+    /// MovieQuotes Class to represent MovieQuote from XML file
+    /// </summary>
+    public class MovieQuote
+    {
+        public string Text { get; set; }
+        public string Movie { get; set; }
+        public string ImdbUrl { get; set; }
+    }
+    /// <summary>
+    /// Reader Class to read quotes from XML file
+    /// </summary>
+    public class QuotesReader
+    {
+        public List<MovieQuote> GetMovieQuotes()
+        {
+            var xdoc = XDocument.Load("MovieQuotes.xml");
+            var quotesList = from x in xdoc.Descendants("Quote")
+                select new MovieQuote
+                {
+                    Text = x.Element("Text").Value,
+                    Movie = x.Element("Movie").Value,
+                    ImdbUrl = x.Element("IMDB").Value
+                };
+            return quotesList.ToList();
+        }
+    }
+    /// <summary>
+    /// Extension Class to load Random/Next Quote
+    /// </summary>
+    public static class Extenders
+    {
+        public static T GetSingleRandom<T>(this IEnumerable<T> target)
+        {
+            Random r = new Random(DateTime.Now.Millisecond);
+            int position = r.Next(target.Count<T>());
+            return target.ElementAt<T>(position);
+        }
+    }
+}
